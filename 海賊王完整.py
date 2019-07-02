83f51368ac189a2de6aaedcbfc39d31dc112ad08
wuchenwei <js.870221@gmail.com>
2019-07-02 17:20:50 +0800

    onepiece_full modified delete useless files

--- 海賊王完整.py
+++ 海賊王完整.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import string
 import os
+import time
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
 target = 'https://one-piece.cn/comic/'   #ptt-NBA-board第一頁
@@ -12,7 +13,7 @@ if (os.path.exists(folder_path) == False): #判斷資料夾是否存在
 	os.makedirs(folder_path) #Create folder
 
 
-chapters = soup.find_all('div' , 'chapter')
+chapters = soup.find_all('div' , id = 'chapter12')
 j = 0;
 for chapter in chapters :
 	j += 1
@@ -22,18 +23,21 @@ for chapter in chapters :
 	a_tags = chapter.find_all('a')
 	i = 0
 	for a in a_tags :
-		ep_url = 'https://one-piece.cn/comic/' + a['href']
+		ep_url = 'https://one-piece.cn/' + a['href']
+		print(ep_url)
 		req = requests.get(url = ep_url , headers = headers)
 		ep = BeautifulSoup(req.text,'html.parser')
 		all_p = ep.find_all('p')
+		print(all_p)
 		for p in all_p :
+			print(p)
 			i += 1
-			src = p.img['src']
-			img_byte = requests.get(src).content
-			file = open(folder_path + str(i) + '.png' , 'wb')
-			file.write(img_byte)
-			
-			file.close()
-			print('第'+str(i)+'張圖片下載完成')
-			#img = requests.get(url).content	
+			if(p.img != None):
+				src = p.img['src']
+				img_byte = requests.get(src).content
+				file = open(folder_path + str(i) + '.png' , 'wb')
+				file.write(img_byte)
+				file.close()
+				print('第'+str(i)+'張圖片下載完成')
+				#img = requests.get(url).content	
 
