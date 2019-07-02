b387f44fbad53d1b72e8ade5932cc702200a1d50
hostxg1143 <44031946+hostxg1143@users.noreply.github.com>
2019-07-02 19:51:59 +0800

    Update 海賊王完整.py

--- 海賊王完整.py
+++ 海賊王完整.py
@@ -3,41 +3,49 @@ from bs4 import BeautifulSoup
 import string
 import os
 import time
+import shutil
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-target = 'https://one-piece.cn/comic/'   #ptt-NBA-board第一頁
+target = 'https://one-piece.cn/comic/'
 req = requests.get(url=target,headers=headers)
 soup = BeautifulSoup(req.text,'html.parser')
-folder_path = 'C:\\Users\\吳振瑋\\Desktop\\真的會用到的\\海賊王圖片\\'
-if (os.path.exists(folder_path) == False): #判斷資料夾是否存在
-	os.makedirs(folder_path) #Create folder
+folder_path = 'one_piece/'
+if os.path.exists(folder_path):
+	shutil.rmtree(folder_path)
+else:
+	os.makedirs(folder_path)
 
-
-chapters = soup.find_all('div' , id = 'chapter12')
+chapters = soup.find('div' , id = 'chapter12')
 j = 0;
-for chapter in chapters :
-	j += 1
-	folder_path = 'C:\\Users\\吳振瑋\\Desktop\\真的會用到的\\海賊王圖片\\' + str(j) + '\\'
-	if (os.path.exists(folder_path) == False) : #判斷資料夾是否存在
-		os.makedirs(folder_path) #Create folder
-	a_tags = chapter.find_all('a')
+
+
+a_tags = chapters.find_all('a')
+#print(a.string.encode(req.encoding).decode('utf-8'))
+
+now = 0
+for a in a_tags :
+	now += 1
+	folder_path = 'one_piece/' + a.string.encode(req.encoding).decode('utf-8') + '/'
+	if os.path.exists(folder_path):
+		shutil.rmtree(folder_path)
+	else:
+		os.makedirs(folder_path)
+	ep_url = 'https://one-piece.cn/' + a['href']
+	#print(ep_url)
+	req = requests.get(url = ep_url , headers = headers)
+	ep = BeautifulSoup(req.text,'html.parser')
+	all_p = ep.find_all('p')
+	#print(all_p)
 	i = 0
-	for a in a_tags :
-		ep_url = 'https://one-piece.cn/' + a['href']
-		print(ep_url)
-		req = requests.get(url = ep_url , headers = headers)
-		ep = BeautifulSoup(req.text,'html.parser')
-		all_p = ep.find_all('p')
-		print(all_p)
-		for p in all_p :
-			print(p)
-			i += 1
-			if(p.img != None):
-				src = p.img['src']
-				img_byte = requests.get(src).content
-				file = open(folder_path + str(i) + '.png' , 'wb')
-				file.write(img_byte)
-				file.close()
-				print('第'+str(i)+'張圖片下載完成')
-				#img = requests.get(url).content	
+	for p in all_p :
+		#print(p)
+		i += 1
+		if(p.img != None):
+			src = p.img['src']
+			img_byte = requests.get(src).content
+			file = open(folder_path + str(i).zfill(2) + '.png' , 'wb')
+			file.write(img_byte)
+			file.close()
+			print('進度:'+str(now)+'/'+str(len(a_tags))+'   '+str(i)+'/'+str(len(all_p)))
+			#img = requests.get(url).content	
 
