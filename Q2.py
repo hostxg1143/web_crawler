3bf693518e6767b18c6a9694093ef9f47ed8a3c6
wuchenwei <js.870221@gmail.com>
2019-07-05 06:34:14 +0800

    add Q1 Q2 Q3 Q4

---
+++ Q2.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import string
+import os
+import shutil
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+target = 'https://www.ptt.cc/bbs/NBA/index.html'   #ptt-NBA-board第一頁
+req = requests.get(url=target,headers=headers)
+soup = BeautifulSoup(req.text,'html.parser')
+
+#獲得下一頁的網址-------(Q2)
+next_page ='https://www.ptt.cc'+soup.find('div','btn-group btn-group-paging').find_next('a','btn wide').find_next_sibling('a')['href']
+
+print(next_page)
---
+++ Q4.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+import string
+import os
+import shutil
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+target = 'https://www.ptt.cc/bbs/NBA/index.html'   #ptt-NBA-board第一頁
+req = requests.get(url=target,headers=headers)
+soup = BeautifulSoup(req.text,'html.parser')
+
+#獲取本頁所有貼文的資訊-------(Q1 ans)
+title = soup.find_all('div','title')
+
+for i in range(len(title)):
+	if(title[i].find('a')!=None):
+		#選出此時的貼文資訊並提取出網址-------(Q3 ans)
+		href = 'https://www.ptt.cc'+title[i].find('a')['href']
+		target = href
+		req = requests.get(url=target,headers=headers)
+		soup = BeautifulSoup(req.text,'html.parser')
+		#獲得貼文內容-------(Q4)
+		content = soup.find('div',id="main-container").text
+		print(content)
