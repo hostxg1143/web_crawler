f54019f75876c4f710ce4a6b05619ba870ea13d5
wuchenwei <js.870221@gmail.com>
2019-07-05 10:52:38 +0800

    various changes

---
+++ 小練習/小練習2.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import string
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+target = 'https://practice2.1mb.site/'   #ptt-NBA-board第一頁
+req = requests.get(url=target,headers=headers)
+soup = BeautifulSoup(req.text,'html.parser')
+
+href = soup.find('a')['href']
+print(href)
+
+
+
